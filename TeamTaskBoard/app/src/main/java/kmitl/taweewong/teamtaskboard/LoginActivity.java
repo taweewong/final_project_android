c79e409795ab71a658a243a8b6b4b26aa0b515cf
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-12 18:28:17 +0700

    implemented OnLoginFacebookComplete in class LoginActivity

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/LoginActivity.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/LoginActivity.java
@@ -1,15 +1,22 @@
 package kmitl.taweewong.teamtaskboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.Profile;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import kmitl.taweewong.teamtaskboard.services.LoginService;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginService.OnLoginFacebookComplete {
+    LoginService loginService;
+    CallbackManager callbackManager;
 
     @BindView(R.id.facebookLoginButton) Button facebookLoginButton;
 
@@ -19,10 +26,34 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        callbackManager = CallbackManager.Factory.create();
+        loginService = new LoginService(this, callbackManager, this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @OnClick(R.id.facebookLoginButton)
     public void loginWithFacebook() {
-        Toast.makeText(this, "text butter knife", Toast.LENGTH_SHORT).show();
+        loginService.loginFacebook();
+    }
+
+    @Override
+    public void onLoginFacebookSuccess(Profile facebookProfile) {
+        Toast.makeText(this, "login: " + facebookProfile.getFirstName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoginFacebookFailed() {
+        Toast.makeText(this, "cancel", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoginFacebookCancelled() {
+        Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show();
     }
 }
