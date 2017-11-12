3847575fa991ec074be5a1329e1045cbdb573d6c
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-12 17:06:37 +0700

    prepared facebook login

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/LoginActivity.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/LoginActivity.java
@@ -1,13 +1,28 @@
 package kmitl.taweewong.teamtaskboard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.facebookLoginButton) Button facebookLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.facebookLoginButton)
+    public void loginWithFacebook() {
+        Toast.makeText(this, "text butter knife", Toast.LENGTH_SHORT).show();
     }
 }
