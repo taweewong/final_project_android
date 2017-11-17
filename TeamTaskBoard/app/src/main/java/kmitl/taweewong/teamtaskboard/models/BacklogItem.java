8fbe7be636c24304ea1ad07df51c92a50287be15
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-18 01:40:48 +0700

    created class BacklogItem

---
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/models/BacklogItem.java
@@ -0,0 +1,22 @@
+package kmitl.taweewong.teamtaskboard.models;
+
+class BacklogItem {
+    private String title;
+    private Tasks tasks;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Tasks getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Tasks tasks) {
+        this.tasks = tasks;
+    }
+}
