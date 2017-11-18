acdf7bb4e921f5ab369cf294971ce403beaff96a
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-18 21:31:16 +0700

    class User implement Parcelable

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/models/User.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/models/User.java
@@ -1,11 +1,48 @@
 package kmitl.taweewong.teamtaskboard.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class User {
+public class User implements Parcelable {
     private String firstName;
     private String lastName;
     private List<String> projects;
+
+    public User() {
+
+    }
+
+    private User(Parcel in) {
+        firstName = in.readString();
+        lastName = in.readString();
+        projects = in.createStringArrayList();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(firstName);
+        dest.writeString(lastName);
+        dest.writeStringList(projects);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 
     public String getFirstName() {
         return firstName;
