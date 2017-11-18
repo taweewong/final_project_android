693723ac9175ade1bf2174b8c7bc53954eecea08
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-18 17:53:51 +0700

    added method initializeFragment in class ProjectActivity

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/activities/ProjectActivity.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/activities/ProjectActivity.java
@@ -1,9 +1,11 @@
 package kmitl.taweewong.teamtaskboard.controllers.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import kmitl.taweewong.teamtaskboard.R;
+import kmitl.taweewong.teamtaskboard.controllers.fragments.ShowProjectsFragment;
 
 public class ProjectActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class ProjectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
+        setTitle(getString(R.string.project_activity));
+
+        initializeFragment();
+    }
+
+    private void initializeFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.projectFragmentContainer, new ShowProjectsFragment())
+                .commit();
     }
 }
