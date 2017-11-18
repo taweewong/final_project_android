7a0f72a030ddfa3fd66d5eb516ad632d1f243ae9
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-18 14:34:33 +0700

    created class ProjectActivity

---
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/activities/ProjectActivity.java
@@ -0,0 +1,15 @@
+package kmitl.taweewong.teamtaskboard.controllers.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kmitl.taweewong.teamtaskboard.R;
+
+public class ProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project);
+    }
+}
