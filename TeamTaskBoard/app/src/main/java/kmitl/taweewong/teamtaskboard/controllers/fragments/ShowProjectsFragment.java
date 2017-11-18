bcbf4e29bc7888f9475d45e1ca0f2cd7a0ebc78a
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-18 14:45:47 +0700

    created class ShowProjectsFragment

---
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/fragments/ShowProjectsFragment.java
@@ -0,0 +1,23 @@
+package kmitl.taweewong.teamtaskboard.controllers.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kmitl.taweewong.teamtaskboard.R;
+
+public class ShowProjectsFragment extends Fragment {
+
+    public ShowProjectsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_show_projects, container, false);
+    }
+}
