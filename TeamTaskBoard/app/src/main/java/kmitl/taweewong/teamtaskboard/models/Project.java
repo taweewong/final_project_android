f2e28fe9df95c9c641057227f29e4765bb165dee
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-18 16:03:48 +0700

    class project implement Parcelable

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/models/Project.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/models/Project.java
@@ -1,11 +1,44 @@
 package kmitl.taweewong.teamtaskboard.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class Project {
+public class Project implements Parcelable {
     private String name;
     private List<String> members;
     private List<BacklogItem> backlogItems;
+
+    public static final String PROJECT_CLASS_KEY = "project";
+
+    private Project(Parcel in) {
+        name = in.readString();
+        members = in.createStringArrayList();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeStringList(members);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Project> CREATOR = new Creator<Project>() {
+        @Override
+        public Project createFromParcel(Parcel in) {
+            return new Project(in);
+        }
+
+        @Override
+        public Project[] newArray(int size) {
+            return new Project[size];
+        }
+    };
 
     public String getName() {
         return name;
@@ -21,5 +54,13 @@ public class Project {
 
     public void setMembers(List<String> members) {
         this.members = members;
+    }
+
+    public List<BacklogItem> getBacklogItems() {
+        return backlogItems;
+    }
+
+    public void setBacklogItems(List<BacklogItem> backlogItems) {
+        this.backlogItems = backlogItems;
     }
 }
