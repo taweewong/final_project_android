6cd1f73afa90da9bc300b9e46b16b6dbb4a3e356
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-19 23:26:51 +0700

    hide action bar menu when start AddProjectFragment

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/fragments/AddProjectFragment.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/fragments/AddProjectFragment.java
@@ -2,8 +2,11 @@ package kmitl.taweewong.teamtaskboard.controllers.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,6 +19,17 @@ public class AddProjectFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.findItem(R.id.addProjectMenu).setVisible(false);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
