b03cfb8783fe7a599507907816f2afcdc92ecc33
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-22 14:15:26 +0700

    created class BacklogItemAdapter

---
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/adapters/BacklogItemAdapter.java
@@ -0,0 +1,21 @@
+package kmitl.taweewong.teamtaskboard.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class BacklogItemAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
