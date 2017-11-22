5ff56d2139b696241b026303ac00e634e84d6512
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-22 14:57:51 +0700

    implemented Parcelable in class BacklogItem

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/models/BacklogItem.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/models/BacklogItem.java
@@ -1,8 +1,43 @@
 package kmitl.taweewong.teamtaskboard.models;
 
-public class BacklogItem {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class BacklogItem implements Parcelable {
     private String title;
     private Tasks tasks;
+
+    public static String BACKLOG_ITEM_CLASS_KEY = "backlogItem";
+
+    public BacklogItem() {
+
+    }
+
+    private BacklogItem(Parcel in) {
+        title = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<BacklogItem> CREATOR = new Creator<BacklogItem>() {
+        @Override
+        public BacklogItem createFromParcel(Parcel in) {
+            return new BacklogItem(in);
+        }
+
+        @Override
+        public BacklogItem[] newArray(int size) {
+            return new BacklogItem[size];
+        }
+    };
 
     public String getTitle() {
         return title;
