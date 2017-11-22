e67c8780c78609b9dd4ad1bfcf733b47faf021c7
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-23 01:25:23 +0700

    implemented onClick and onEditComplete on class BacklogItemActivity

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/activities/BacklogItemActivity.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/activities/BacklogItemActivity.java
@@ -14,14 +14,19 @@ import com.google.firebase.auth.FirebaseAuth;
 import java.util.ArrayList;
 
 import kmitl.taweewong.teamtaskboard.R;
+import kmitl.taweewong.teamtaskboard.adapters.BacklogItemAdapter;
 import kmitl.taweewong.teamtaskboard.controllers.fragments.AddBacklogItemFragment;
+import kmitl.taweewong.teamtaskboard.controllers.fragments.EditBacklogItemFragment;
 import kmitl.taweewong.teamtaskboard.controllers.fragments.ShowBacklogItemsFragment;
 import kmitl.taweewong.teamtaskboard.models.BacklogItem;
 import kmitl.taweewong.teamtaskboard.models.Project;
 import kmitl.taweewong.teamtaskboard.services.DatabaseService;
 
 public class BacklogItemActivity extends AppCompatActivity implements
-        DatabaseService.OnQueryBacklogItemsCompleteListener, AddBacklogItemFragment.OnAddBacklogItemCompleteListener {
+        DatabaseService.OnQueryBacklogItemsCompleteListener,
+        AddBacklogItemFragment.OnAddBacklogItemCompleteListener,
+        BacklogItemAdapter.OnClickBacklogItemListener,
+        EditBacklogItemFragment.OnEditBacklogItemCompleteListener {
     private ArrayList<BacklogItem> backlogItems;
     private Project project;
     private String projectId;
@@ -80,6 +85,22 @@ public class BacklogItemActivity extends AppCompatActivity implements
         getSupportFragmentManager().popBackStack();
     }
 
+    @Override
+    public void onClickBacklogItem(int position) {
+        Toast.makeText(this, "click " + backlogItems.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLongClickBacklogItem(int position) {
+        replaceEditBacklogItemFragment(position);
+    }
+
+    @Override
+    public void onEditBacklogItemComplete(int position, BacklogItem editedBacklogItem) {
+        this.backlogItems.set(position, editedBacklogItem);
+        getSupportFragmentManager().popBackStack();
+    }
+
     private void initializeFragment(ArrayList<BacklogItem> backlogItems) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
@@ -96,6 +117,18 @@ public class BacklogItemActivity extends AppCompatActivity implements
                         R.anim.slide_in_from_left,
                         R.anim.slide_out_to_right)
                 .replace(R.id.backlogItemFragmentContainer, AddBacklogItemFragment.newInstance(project))
+                .addToBackStack(null)
+                .commit();
+    }
+
+    private void replaceEditBacklogItemFragment(int position) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.slide_in_from_right,
+                        R.anim.slide_out_to_left,
+                        R.anim.slide_in_from_left,
+                        R.anim.slide_out_to_right)
+                .replace(R.id.backlogItemFragmentContainer, EditBacklogItemFragment.newInstance(project, position))
                 .addToBackStack(null)
                 .commit();
     }
