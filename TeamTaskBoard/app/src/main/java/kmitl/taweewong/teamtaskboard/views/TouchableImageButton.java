8e474fe9e16727793cabfa066904d4b9f4ee2f74
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-23 23:03:34 +0700

    created class TouchableImageButton and changed type ImageView to TouchableImageButton

---
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/views/TouchableImageButton.java
@@ -0,0 +1,23 @@
+package kmitl.taweewong.teamtaskboard.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class TouchableImageButton extends android.support.v7.widget.AppCompatImageButton {
+    public TouchableImageButton(Context context) {
+        super(context);
+    }
+
+    public TouchableImageButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TouchableImageButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+}
