49e9ed823d2e2137fa80d394624b0d52093ffd2e
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-24 21:31:49 +0700

    edited all getters method in class Tasks

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/models/Tasks.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/models/Tasks.java
@@ -1,33 +1,78 @@
 package kmitl.taweewong.teamtaskboard.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class Tasks {
+public class Tasks implements Parcelable {
     private List<Task> todoTasks;
     private List<Task> doingTasks;
     private List<Task> doneTasks;
 
-    public List<Task> getTodoTasks() {
-        return todoTasks;
+    public static String TASKS_CLASS_KEY = "tasks";
+
+    Tasks() {
     }
+
+    private Tasks(Parcel in) {
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Tasks> CREATOR = new Creator<Tasks>() {
+        @Override
+        public Tasks createFromParcel(Parcel in) {
+            return new Tasks(in);
+        }
+
+        @Override
+        public Tasks[] newArray(int size) {
+            return new Tasks[size];
+        }
+    };
 
     public void setTodoTasks(List<Task> todoTasks) {
         this.todoTasks = todoTasks;
-    }
-
-    public List<Task> getDoingTasks() {
-        return doingTasks;
     }
 
     public void setDoingTasks(List<Task> doingTasks) {
         this.doingTasks = doingTasks;
     }
 
-    public List<Task> getDoneTasks() {
-        return doneTasks;
-    }
-
     public void setDoneTasks(List<Task> doneTasks) {
         this.doneTasks = doneTasks;
+    }
+
+    public List<Task> getTodoTasks() {
+        if (todoTasks == null) {
+            return new ArrayList<>();
+        }
+
+        return todoTasks;
+    }
+
+    public List<Task> getDoingTasks() {
+        if (doingTasks == null) {
+            return new ArrayList<>();
+        }
+
+        return doingTasks;
+    }
+
+    public List<Task> getDoneTasks() {
+        if (doneTasks == null) {
+            return new ArrayList<>();
+        }
+
+        return doneTasks;
     }
 }
