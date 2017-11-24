49f19f16bb44789c12f2f1c23c98ae5f43c86dd8
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-25 01:13:24 +0700

    implemented OnQueryTasksCompleteListener and OnClickTaskListener in class TaskActivity

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/activities/TaskActivity.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/activities/TaskActivity.java
@@ -6,25 +6,65 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import kmitl.taweewong.teamtaskboard.R;
+import kmitl.taweewong.teamtaskboard.adapters.TaskItemAdapter;
 import kmitl.taweewong.teamtaskboard.adapters.TaskPagerAdapter;
+import kmitl.taweewong.teamtaskboard.models.Tasks;
+import kmitl.taweewong.teamtaskboard.services.DatabaseService;
 
-public class TaskActivity extends AppCompatActivity {
+public class TaskActivity extends AppCompatActivity implements
+        DatabaseService.OnQueryTasksCompleteListener,
+        TaskItemAdapter.OnClickTaskListener {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.tasksContainer) ViewPager viewPager;
+    @BindView(R.id.tabs) TabLayout tabLayout;
+
+    private Tasks tasks;
+    private String projectId;
+    private String itemId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+        ButterKnife.bind(this);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        projectId = getIntent().getStringExtra("projectId");
+        itemId = getIntent().getStringExtra("itemId");
+
+        DatabaseService databaseService = new DatabaseService();
+        databaseService.queryTasks(projectId, itemId, this);
+    }
+
+    @Override
+    public void onQueryTasksItemsSuccess(Tasks tasks) {
+        this.tasks = tasks;
+        initializeTab();
+    }
+
+    @Override
+    public void onQueryTasksItemsFailed() {
+
+    }
+
+    private void initializeTab() {
         setSupportActionBar(toolbar);
-        TaskPagerAdapter taskPagerAdapter = new TaskPagerAdapter(getSupportFragmentManager());
-
-        ViewPager viewPager = findViewById(R.id.container);
+        TaskPagerAdapter taskPagerAdapter = new TaskPagerAdapter(getSupportFragmentManager(), tasks, projectId, itemId);
         viewPager.setAdapter(taskPagerAdapter);
-
-        TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setSelectedTabIndicatorColor(getResources().getColor(android.R.color.white));
+    }
+
+    @Override
+    public void onClickTask(int position) {
+
+    }
+
+    @Override
+    public void onLongClickTask(int position) {
+
     }
 }
