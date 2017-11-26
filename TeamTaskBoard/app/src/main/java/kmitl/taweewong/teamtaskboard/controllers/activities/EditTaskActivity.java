63c10e1125e20df9e43d51f51aeba1248b796987
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-11-26 15:42:40 +0700

    inflated delete task menu in class EditTaskActivity

--- TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/activities/EditTaskActivity.java
+++ TeamTaskBoard/app/src/main/java/kmitl/taweewong/teamtaskboard/controllers/activities/EditTaskActivity.java
@@ -3,7 +3,11 @@ package kmitl.taweewong.teamtaskboard.controllers.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,8 +22,10 @@ import static kmitl.taweewong.teamtaskboard.controllers.activities.TaskActivity.
 import static kmitl.taweewong.teamtaskboard.controllers.activities.TaskActivity.POSITION_KEY;
 import static kmitl.taweewong.teamtaskboard.controllers.activities.TaskActivity.PROJECT_ID_KEY;
 import static kmitl.taweewong.teamtaskboard.controllers.activities.TaskActivity.TASK_KEY;
+import static kmitl.taweewong.teamtaskboard.controllers.activities.TaskActivity.TASK_LIST_KEY;
 import static kmitl.taweewong.teamtaskboard.controllers.activities.TaskActivity.TASK_TYPE_KEY;
-import static kmitl.taweewong.teamtaskboard.controllers.fragments.ShowTasksFragment.EDIT_TASK_REQUEST_CODE;
+import static kmitl.taweewong.teamtaskboard.controllers.fragments.ShowTasksFragment.DELETE_TASK_RESPONSE_CODE;
+import static kmitl.taweewong.teamtaskboard.controllers.fragments.ShowTasksFragment.EDIT_TASK_RESPONSE_CODE;
 
 public class EditTaskActivity extends AppCompatActivity {
 
@@ -31,6 +37,7 @@ public class EditTaskActivity extends AppCompatActivity {
     private String itemId;
     private TaskType taskType;
     private int position;
+    private List<Task> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +51,26 @@ public class EditTaskActivity extends AppCompatActivity {
         itemId = getIntent().getStringExtra(ITEM_ID_KEY);
         taskType = TaskType.valueOf(getIntent().getStringExtra(TASK_TYPE_KEY));
         position = getIntent().getIntExtra(POSITION_KEY, -1);
+        taskList = getIntent().getParcelableArrayListExtra(TASK_LIST_KEY);
 
         editTaskEditText.setText(task.getTitle());
         editTaskDescriptionEditText.setText(task.getDescription());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.edit_task_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.deleteTaskMenu:
+                deleteTask();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @OnClick(R.id.editTaskButton)
@@ -60,7 +84,7 @@ public class EditTaskActivity extends AppCompatActivity {
 
         databaseService.editTask(task, projectId, itemId, taskType);
 
-        sendDataBackToCaller(task);
+        sendEditedDataBackToCaller(task);
     }
 
     @Override
@@ -69,11 +93,27 @@ public class EditTaskActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right);
     }
 
-    private void sendDataBackToCaller(Task editedTask) {
+    private void sendEditedDataBackToCaller(Task editedTask) {
         Intent result = new Intent();
         result.putExtra(EDITED_TASK_KEY, editedTask);
         result.putExtra(POSITION_KEY, position);
-        setResult(EDIT_TASK_REQUEST_CODE, result);
+        setResult(EDIT_TASK_RESPONSE_CODE, result);
+        finish();
+        overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right);
+    }
+
+    private void deleteTask() {
+        DatabaseService databaseService = new DatabaseService();
+
+        taskList.remove(position);
+        databaseService.deleteTask(taskList, task.getId(), projectId, itemId, taskType);
+        sendDeletedPositionBackToCaller(position);
+    }
+
+    private void sendDeletedPositionBackToCaller(int position) {
+        Intent result = new Intent();
+        result.putExtra(POSITION_KEY, position);
+        setResult(DELETE_TASK_RESPONSE_CODE, result);
         finish();
         overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right);
     }
